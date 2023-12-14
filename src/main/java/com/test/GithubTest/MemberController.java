d07d49e56242d2c7812e912b876269bf097cc9d3
Jinu Yi <dionisos198@naver.com>
2023-12-14 18:51:49 +0900

    feat: afterNoonLocal1

--- src/main/java/com/test/GithubTest/MemberController.java
+++ src/main/java/com/test/GithubTest/MemberController.java
@@ -97,6 +97,9 @@ public class MemberController {
     public void upstreamPullCheck8(){
         System.out.println("upstream Pull Check");
     }
+    public void afterNoonLocal1(){
+        System.out.println("afterNoonLocal1");
+    }
 
 
 
