3a7aa1324547f2fce6a61758ee2497313d7ee9bc
Jinu Yi <dionisos198@naver.com>
2023-12-17 04:12:09 +0900

    feat: B 구현

---
+++ src/main/java/com/test/GithubTest/JinuSecondController.java
@@ -0,0 +1,10 @@
+package com.test.GithubTest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JinuSecondController {
+    public void functionB(){
+        System.out.println("functionB 기능 구현! ");
+    }
+}
