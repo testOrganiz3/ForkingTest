552e3327b96b19e656830d630b5c191eebd85e89
Jinu Yi <dionisos198@naver.com>
2023-12-17 04:18:44 +0900

    refactor: A 코드 수정 완료!

--- src/main/java/com/test/GithubTest/JinuController.java
+++ src/main/java/com/test/GithubTest/JinuController.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class JinuController {
     public void FunctionA(){
         System.out.println("A 기능 구현 성공!!! pull request하구 merge 기다리는 중 !! ㅎㅎ");
+        System.out.println(" 코드를 갈아 엎었다");
     }
 }
