5efcae64b95244506cf0fcc26ad3aa107c8c7ca4
Jinu Yi <dionisos198@naver.com>
2023-12-18 18:42:41 +0900

    feat: hi

--- src/main/java/com/test/GithubTest/MemberController.java
+++ src/main/java/com/test/GithubTest/MemberController.java
@@ -146,6 +146,15 @@ public class MemberController {
     public void kkkkkkkkkFake3(){
         System.out.println("kkkkkkkkkFake");
     }
+    public void hi(){
+        System.out.println("안녕안녕하세요");
+        System.out.println("안녕안녕하세요");
+        System.out.println("안녕안녕하세요");
+        System.out.println("안녕안녕하세요");
+        System.out.println("안녕안녕하세요");
+        System.out.println("안녕안녕하세요");
+        System.out.println("안녕안녕하세요");
+    }
 
 
 
