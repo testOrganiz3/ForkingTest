622f496010dba25a7ee234979bc4336df7cdc791
Jinu Yi <dionisos198@naver.com>
2023-12-23 15:39:10 +0900

    Feat: Clone 의 BothModified

--- src/main/java/com/test/GithubTest/BothConflictModifyController.java
+++ src/main/java/com/test/GithubTest/BothConflictModifyController.java
@@ -4,4 +4,7 @@ public class BothConflictModifyController {
     public void printBothConflictModifyController(){
         System.out.println("Clone:BothConflictModifyController");
     }
+    public void BothModifiedClone(){
+        System.out.println("Clone: BothModified");
+    }
 }
