d774575bc18ae66aa0addb02f2b2ff41b235643d
Jinu Yi <dionisos198@naver.com>
2023-12-23 15:38:34 +0900

    Feat: Clone 의 AcceptTheirs

--- src/main/java/com/test/GithubTest/AcceptTheirsController.java
+++ src/main/java/com/test/GithubTest/AcceptTheirsController.java
@@ -4,4 +4,7 @@ public class AcceptTheirsController {
     public void AcceptTheirsController(){
         System.out.println("Clone: AcceptTheirsController");
     }
+    public void AcceptTheirsClone(){
+        System.out.println("Clone: AcceptTheirs");
+    }
 }
