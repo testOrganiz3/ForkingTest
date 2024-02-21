203cf2fa2d61caf3d3c4211696c1a7bcb1dc01a4
Jinu Yi <dionisos198@naver.com>
2024-02-22 04:02:30 +0900

    Feat : 내가 개발한 AcceptYoursFork

--- src/main/java/com/test/GithubTest/AcceptYoursController.java
+++ src/main/java/com/test/GithubTest/AcceptYoursController.java
@@ -4,4 +4,7 @@ public class AcceptYoursController {
     public void AcceptYoursController(){
         System.out.println("Clone:AcceptYoursController");
     }
+    public void AcceptYoursFork(){
+        System.out.println("Fork: AcceptYours");
+    }
 }
